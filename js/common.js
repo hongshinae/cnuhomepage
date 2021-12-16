570962a151d5e23d2605f95617c8a1e5a2ff7964
hong <tlsdoakstp@gmail.com>
2021-12-16 15:29:02 +0900

    update
    Signed-off-by: hong <tlsdoakstp@gmail.com>

---
+++ js/common.js
@@ -0,0 +1,24 @@
+
+
+// $('.dep1_link').click(function(){
+// alert("hi");
+// });
+
+  // $('.dep1_link').click(function(){
+  //     $('.header').addClass('over'); 
+  // });
+ 
+
+
+
+
+
+
+
+// window.onload = function(){
+//   $(".dep1_link").mouseover(function () { 
+//     $('.header').addClass('over'); 
+//   }).mouseout(function(){ 
+//     $('.header').removeClass('over'); 
+//   })
+// }
