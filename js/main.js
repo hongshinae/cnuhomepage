50e38f429ef5d78eb953866ddc88e4071250abbd
hong <tlsdoakstp@gmail.com>
2022-04-15 15:14:30 +0900

    f

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-let maintext = document.querySelector("h1");
+
 
 		window.addEventListener("scroll", function() {
 			let value = window.scrollY;
 			console.log("scrollY", value);
 			if (value > 300) {
-				document.maintext.style.display = "none"
+				document.getElementById("#dd").style.display = "none";
 			}
-        
-	// 		} else{
-    //     maintext.style.margin = "200px"
-    //   }
 		}
-		);
+	);
