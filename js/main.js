f0244fb7c078cb9ac4895bfde77cee8e353cf702
hong <tlsdoakstp@gmail.com>
2022-04-18 08:00:35 +0900

    .
    Signed-off-by: hong <tlsdoakstp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 
-
+let mainCompanyimg = document.querySelector(".main-companyimg .container");
 		window.addEventListener("scroll", function() {
 			let value = window.scrollY;
 			console.log("scrollY", value);
-			if (value > 300) {
-				document.getElementById("#dd").style.display = "none";
+			if (value > 255) {
+				mainCompanyimg.style.animation = "slideInDown 1s ease-in";
+			}
+			else {
+				mainCompanyimg.style.animation = "slideInUp 1s ease-in";
 			}
 		}
 	);
